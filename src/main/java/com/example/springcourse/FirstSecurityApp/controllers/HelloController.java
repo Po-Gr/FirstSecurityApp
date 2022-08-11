8c9d3f9a906d3dd07efd45772cb73a402b09a11a
Polina Grigorovich <grigorovichpk@mail.ru>
2022-08-11 16:45:04 +0300

    Initial commit

---
+++ src/main/java/com/example/springcourse/FirstSecurityApp/controllers/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.springcourse.FirstSecurityApp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String seyHello() {
+        return "hello";
+    }
+}
