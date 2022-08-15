43d43eb850becbcafccb98ea628873be1995dc8d
Polina Grigorovich <grigorovichpk@mail.ru>
2022-08-15 16:47:47 +0300

    Стандартная аутентификация

--- src/main/java/com/example/springcourse/FirstSecurityApp/controllers/HelloController.java
+++ src/main/java/com/example/springcourse/FirstSecurityApp/controllers/HelloController.java
@@ -1,12 +1,26 @@
 package com.example.springcourse.FirstSecurityApp.controllers;
 
+import com.example.springcourse.FirstSecurityApp.security.PersonDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
     @GetMapping("/hello")
     public String seyHello() {
+        return "hello";
+    }
+
+    @GetMapping("/showUserInfo")
+    public String showUserInfo(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails)authentication.getPrincipal();
+        System.out.println(personDetails.getPerson());
+
+        model.addAttribute(personDetails.getPerson());
         return "hello";
     }
 }
